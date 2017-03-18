d7f90d2ebc37569e60a43f00c2b231327595e40c
Tenrys <tenrys.iaido@live.fr>
2017-03-18 17:40:23 +0100

    nuke the default admin mod

--- lua/mingeban/sv_init.lua
+++ lua/mingeban/sv_init.lua
@@ -7,5 +7,10 @@ AddCSLuaFile("cl_commands.lua")
 include("sv_ranks.lua")
 include("sv_commands.lua")
 
+hook.Add("Initialize", "mingeban-nukedefault", function()
+	hook.Remove("PlayerInitialSpawn", "PlayerAuthSpawn")
+	hook.Remove("Initialize", "mingeban-nukedefault")
+end)
+
 MsgC(Color(127, 255, 127), "[mingeban]") MsgC(Color(255, 255, 255), " Server side loaded\n")
 
